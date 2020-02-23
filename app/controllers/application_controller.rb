84d16305334572fe7e27e18a922736c09cb6fb0b
Jamal Frank <jamaljfrank@gmail.com>
2020-02-23 07:10:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    user = User.find_by(:username => params[:username])
+    user = User.find_by(:username => params[:username], :password => params[:password])
  
   if user && user.authenticate(params[:password])
     session[:id] = user.id
