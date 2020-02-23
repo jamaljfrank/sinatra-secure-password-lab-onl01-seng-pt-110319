21fc40cc795c4c3f292f9f092021f3d89ff82618
Jamal Frank <jamaljfrank@gmail.com>
2020-02-23 05:54:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    @user = User.new(:username => params[:username], :password => params[:password])
  
-    if @user.username 
+    if params[:username] 
       redirect "/login"
     else
       redirect "/failure"
